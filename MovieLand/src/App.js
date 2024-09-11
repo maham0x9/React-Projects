3ba9290e14e9f378ce58f63d4dabc94ef87d2155
Maham Nadeem <156057831+maham0x9@users.noreply.github.com>
2024-09-11 20:12:41 +0500

    Update App.js

--- MovieLand/src/App.js
+++ MovieLand/src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=f1b154d4";
+const API_URL = "https://www.omdbapi.com?apikey=f1b154d4";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
